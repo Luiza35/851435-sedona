957d52f9f63c36dfa9a662f84e5c9ac34c88fe10
Luiza35 <43570511+Luiza35@users.noreply.github.com>
2018-12-14 17:09:42 +0300

    JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 var link = document.querySelector(".search-link");
 var popup = document.querySelector(".form-search");
+var form = popup.querySelector("form");
+var date-arrival = popup.querySelector("[name=date-arrival]");
+var date-departure = popup.querySelector("[name=date-departure]");
+var quantity-adults = popup.querySelector("[name=quantity-adults]");
+var quantity-children = popup.querySelector("[name=quantity-children]");
+
+
 
   popup.classList.remove("form-search-show");
-  console.log("link");
+  
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("form-search-show");
-    console.log("tutu");
+});
+
+	form.addEventListener("submit", function (evt) {
+  	if (!date-arrival.value || !date-departure.value || !quantity-adults.value || !quantity-children.value) {
+  	evt.preventDefault();
+  	popup.classList.add("form-search-show-error");
+  }
 });
 
 
