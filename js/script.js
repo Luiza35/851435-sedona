48a3605f6b51fb86dc5abb22cec25614ce0df256
Luiza35 <43570511+Luiza35@users.noreply.github.com>
2018-12-17 22:07:01 +0300

    Пиксель Перфект

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 var link = document.querySelector(".search-link");
 var popup = document.querySelector(".form-search");
-
 var datearrival = popup.querySelector("[name=date-arrival]");
 var datedeparture = popup.querySelector("[name=date-departure]");
 var quantityadults = popup.querySelector("[name=quantity-adults]");
 var quantitychildren = popup.querySelector("[name=quantity-children]");
 
-
-
-  popup.classList.remove("form-search-show");
+popup.classList.remove("form-search-show");
   
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
